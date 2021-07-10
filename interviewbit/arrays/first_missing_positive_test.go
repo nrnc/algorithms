b034228373e99124d18523a0f8da90d39e6b9256
nchukkaio <narendra.chukka@yahoo.in>
2021-07-10 21:35:29 +0530

    solved interviewbit arrays first missing positive

---
+++ interviewbit/arrays/first_missing_positive_test.go
@@ -0,0 +1,32 @@
+package arrays
+
+import "testing"
+
+type Problem1 struct {
+	input  []int
+	output int
+}
+
+func TestFirstMissingPositive(t *testing.T) {
+	testCases := []Problem1{
+		{
+			input:  []int{1, 2, 0},
+			output: 3,
+		},
+		{
+			input:  []int{3, 4, -1, 1},
+			output: 2,
+		},
+		{
+			input:  []int{-8, -7, -6},
+			output: 1,
+		},
+	}
+
+	for _, test := range testCases {
+		ans := FirstMissingPositive(test.input)
+		if ans != test.output {
+			t.Errorf("expected %d got %d", test.output, ans)
+		}
+	}
+}
